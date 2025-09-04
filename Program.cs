28dbf1a4c10b0286f9366cc6a710457e9ce747ad
Felipe-Marcos-De-Souza <Felipinhomarcos582@gmail.com>
2025-09-04 13:45:02 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,23 @@ class Program
     {
 
     }
+     
+public abstract class Criatura
+{
+    public string nome {get; set;}
+    public int vida {get; set;}
+    public int ataque {get; set;}
+    public int defesa {get; set;}
 
+    public virtual void atacar ()
+    {
+
+    }
+
+    public void receberDano(int dano)
+    {
+        int dano = ataque - defesa 
+    }
+}
 
 }
